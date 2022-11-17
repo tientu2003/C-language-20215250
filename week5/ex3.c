4cced6d015015d02596ec232484879cf94e94fad
tientu2003 <117565347+tientu2003@users.noreply.github.com>
2022-11-18 00:16:39 +0700

    hmm

--- week5/ex3.c
+++ week5/ex3.c
@@ -2,6 +2,18 @@
 
 void main(void)
 {
+    int k,i,n;
+    printf("Input the height of triangle:");
+    scanf("%d",&n);
+     for (i=1;i<=n;++i)
+        {
+        for (k=n-i;k>=0;--k)
+            printf(" ");
+        for (k=0;k<i;++k)
+            printf("*");
+        for (k=i-2;k>=0;--k)
+            printf("*"); 
+        printf("\n");            
+        }
 
-    
 }
